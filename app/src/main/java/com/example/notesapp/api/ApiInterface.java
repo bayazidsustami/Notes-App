f07a51050fbdc4b2fa217c48e55824c5e987130f
bayazidsustami <bayazidsustamy@gmail.com>
2020-02-17 01:00:46 +0800

    menambahkan method GET

--- app/src/main/java/com/example/notesapp/api/ApiInterface.java
+++ app/src/main/java/com/example/notesapp/api/ApiInterface.java
@@ -2,9 +2,12 @@ package com.example.notesapp.api;
 
 import com.example.notesapp.model.Note;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface ApiInterface {
@@ -16,5 +19,8 @@ public interface ApiInterface {
             @Field("note") String note,
             @Field("color") int color
     );
+
+    @GET("notes.php")
+    Call<List<Note>> getNotes();
 
 }
