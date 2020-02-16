f2690d33a523b01a8d56c8167bcbe4229a1aa7f0
bayazidsustami <bayazidsustamy@gmail.com>
2020-02-17 00:57:56 +0800

    implement method MainView

--- app/src/main/java/com/example/notesapp/activity/main/MainActivity.java
+++ app/src/main/java/com/example/notesapp/activity/main/MainActivity.java
@@ -1,26 +1,81 @@
 package com.example.notesapp.activity.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.notesapp.R;
 import com.example.notesapp.activity.editor.EditorActivity;
+import com.example.notesapp.model.Note;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements MainView {
 
     FloatingActionButton fab;
+    RecyclerView recyclerView;
+    SwipeRefreshLayout swipeRefresh;
+
+    MainPresenter presenter;
+    MainAdapter adapter;
+    MainAdapter.ItemClickListener itemClickListener;
+
+    List<Note> note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        swipeRefresh = findViewById(R.id.swipe_refresh);
+        recyclerView = findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         fab = findViewById(R.id.add);
 
         fab.setOnClickListener(view ->
             startActivity(new Intent(this, EditorActivity.class)));
+
+        presenter = new MainPresenter(this);
+        presenter.getDate();
+
+        swipeRefresh.setOnRefreshListener(() -> presenter.getDate());
+
+        itemClickListener = ((view, position) -> {
+            String title = note.get(position).getTitle();
+            Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void showLoading() {
+        swipeRefresh.setRefreshing(true);
+    }
+
+    @Override
+    public void hideLoading() {
+        swipeRefresh.setRefreshing(false);
+    }
+
+    @Override
+    public void onGetResult(List<Note> notes) {
+        adapter = new MainAdapter(this, notes, itemClickListener);
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+        note = notes;
+    }
+
+    @Override
+    public void onErrorLoading(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        Log.d("message error : ", message);
     }
 }
